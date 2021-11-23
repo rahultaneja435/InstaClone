e183f03ca7ce59005b173144e71b0090a7977903
Rahul Taneja <rahultaneja03@gmail.com>
2021-11-23 21:03:37 +0530

    suggestions

--- components/Suggetion.js
+++ components/Suggetion.js
@@ -18,6 +18,16 @@ function Suggetion() {
             <h3 className="text-sm font-bold">suggestions</h3>
             <button className="text-gray-600 font-semibold">See all</button>
             </div>
+        {suggetion.map(ele=>(
+            <div key={ele.id} className="flex items-center justify-between mt-3">
+             <img  className="h-10 w-10 rounded-full p-[2px]" src={ele.avatar}/>
+             <div className="flex-1 ml-4">
+                 <h2 className="text-sm font-semibold">{ele.username}</h2>
+                 <h3 className="text-xs text-gray-400">Works at {ele.company.name}</h3>
+            </div>
+            <button className="text-blue-400 font-bold text-xs">Follow</button>
+            </div>
+        ))}
         </div>
     )
 }
